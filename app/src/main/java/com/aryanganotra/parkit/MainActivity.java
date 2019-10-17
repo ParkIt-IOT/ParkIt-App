98771858c98fe43731854d01e8cd55c4937f38e4
Aryan Ganotra <aryanganotra7@gmail.com>
2019-10-17 13:45:10 +0530

    .

--- app/src/main/java/com/aryanganotra/parkit/MainActivity.java
+++ app/src/main/java/com/aryanganotra/parkit/MainActivity.java
@@ -1,7 +1,12 @@
 package com.aryanganotra.parkit;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button bt = (Button) findViewById(R.id.bt);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ListActivity.class));
+            }
+        });
     }
 }
