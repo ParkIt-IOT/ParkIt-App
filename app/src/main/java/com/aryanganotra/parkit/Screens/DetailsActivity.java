9166c28903581aad2c24bfee5efd3589d9f47400
Aryan Ganotra <aryanganotra7@gmail.com>
2019-10-18 19:42:13 +0530

    .

---
+++ app/src/main/java/com/aryanganotra/parkit/Screens/DetailsActivity.java
@@ -0,0 +1,19 @@
+package com.aryanganotra.parkit.Screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.aryanganotra.parkit.R;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    TextView place, time, 
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
